abb31f213ffb442cd386ff0fa0b1daded9b1470f
Farida Rizaeva <rizaeva@arrival.com>
2022-06-28 01:08:37 +0400

    initial commit

---
+++ src/test/kotlin/FirstFakeTest.kt
@@ -0,0 +1,189 @@
+import org.junit.jupiter.api.Assertions
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class FirstFakeTest {
+
+    private val testSample: Sample = Sample()
+
+    @Test
+    fun sum_12() {
+        val expected = 12
+        Assertions.assertEquals(expected, testSample.sum(6, 6))
+    }
+
+    @Test
+    fun sum_10() {
+        val expected = 10
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_100() {
+        val expected = 100
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_102() {
+        val expected = 102
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_103() {
+        val expected = 103
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_104() {
+        val expected = 104
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_105() {
+        val expected = 105
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_106() {
+        val expected = 106
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_107() {
+        val expected = 107
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_108() {
+        val expected = 108
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_109() {
+        val expected = 109
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_111() {
+        val expected = 111
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_112() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_113() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_114() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_115() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_116() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_117() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_118() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_119() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_120() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_121() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_122() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_123() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_124() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_125() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_126() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_127() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_128() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_129() {
+        val expected = 129
+        Assertions.assertEquals(expected, testSample.sum(120, 9))
+    }
+}
