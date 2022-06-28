704f7b5594f56b634e205b44f93c2aca68322cb2
Farida Rizaeva <rizaeva@arrival.com>
2022-06-28 21:46:31 +0400

    bunch of tests addition

--- src/test/kotlin/SampleTest_0.kt
+++ src/test/kotlin/SampleTest_0.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,4 +14,127 @@ internal class SampleTest_0 {
         assertEquals(expected, testSample.sum(40, 2))
     }
 
+    @Test
+    fun sum_112() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_113() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_114() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_115() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_116() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_117() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_118() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_119() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_120() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_121() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_122() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_123() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_124() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_125() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_126() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_127() {
+        Thread.sleep(10000)
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_128() {
+        val expected = 101
+        Assertions.assertEquals(expected, testSample.sum(6, 4))
+    }
+
+    @Test
+    fun sum_129() {
+        val expected = 129
+        Assertions.assertEquals(expected, testSample.sum(120, 9))
+    }
 }
